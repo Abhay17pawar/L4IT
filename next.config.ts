a9bab1fc1fd2986d95bac4215d0fb640fd9846e7
Abhay Pawar <abhaypawar0817@gmail.com>
2025-05-20 14:47:25 +0530

    disabled eslint

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
    images: {
     domains: ['l4it.net','logos-world.net'], 
   },
+   eslint: {
+    ignoreDuringBuilds: true, 
+  },
 };
 
 export default nextConfig;
